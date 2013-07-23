1fe453285088985cadd7b52d522830e166e43126
Daniel Kirchner <daniel.c.kirchner@googlemail.com>
2013-07-23 19:42:54 +0200

    practicing static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,11 @@ describe "Static Pages" do
 	    	visit '/static_pages/home'
 	    	expect(page).to have_content('Mighty Sample')
 		end
+
+		it "should have the correct title" do
+			visit '/static_pages/home'
+			expect(page).to have_title("Fruchtfabrik | Home")
+		end
 	end
 
 	describe "Help Page" do
@@ -13,12 +18,22 @@ describe "Static Pages" do
 			visit '/static_pages/help'
 			expect(page).to have_content('Help')
 		end
+
+		it "should have the correct title" do
+			visit '/static_pages/help'
+			expect(page).to have_title("Fruchtfabrik | Help")
+		end
 	end
 
 	describe "About Page" do
 		it "should have the content 'About us'" do
-			visit '/static_pages/about_us'
+			visit '/static_pages/about'
 			expect(page).to have_content('About Us')
+		end
+
+		it "should have the correct title" do
+			visit '/static_pages/about'
+			expect(page).to have_title("Fruchtfabrik | About")
 		end
 	end
 end
