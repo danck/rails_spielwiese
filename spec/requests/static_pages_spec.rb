d25574799b44309278a7e0eb2000ae1474b47a26
Daniel Kirchner <daniel.c.kirchner@googlemail.com>
2013-07-23 23:58:38 +0200

    refactoring integration tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,51 +2,34 @@ require 'spec_helper'
 require './spec/requests/shared_static_pages.rb'
 
 describe "Static Pages" do
-	describe "Home page" do
-	 	it "should have the content 'Mighty Sample'" do
-	    	visit '/static_pages/home'
-	    	expect(page).to have_content('Mighty Sample')
-		end
+	subject { page }
 
-		it "should have the correct title" do
-			visit '/static_pages/home'
-			expect(page).to have_title("#{@site_title_prefix}Home")
-		end
+	describe "Home page" do
+		before { visit root_path }
+
+		it { should have_content 'Mighty Sample' }
+		it { should have_title full_title '' } # utiliy methode aus support/utilities.rb
+
 	end
 
 	describe "Help Page" do
-		it "should have the content 'Help'" do
-			visit '/static_pages/help'
-			expect(page).to have_content('Help')
-		end
+		before { visit help_path }
 
-		it "should have the correct title" do
-			visit '/static_pages/help'
-			expect(page).to have_title("#{@site_title_prefix}Help")
-		end
+		it { should have_content 'Help' }
+		it { should have_title "#{@site_title_prefix}Help" }
 	end
 
 	describe "About Page" do
-		it "should have the content 'About us'" do
-			visit '/static_pages/about'
-			expect(page).to have_content('About Us')
-		end
+		before { visit about_path }
 
-		it "should have the correct title" do
-			visit '/static_pages/about'
-			expect(page).to have_title("#{@site_title_prefix}About")
-		end
+		it { should have_content 'About' }
+		it { should have_title "#{@site_title_prefix}About" }
 	end
 
 		describe "Contact Page" do
-		it "should have the content 'About us'" do
-			visit '/static_pages/contact'
-			expect(page).to have_content('Contact Us')
-		end
+		before { visit contact_path }
 
-		it "should have the correct title" do
-			visit '/static_pages/contact'
-			expect(page).to have_title("#{@site_title_prefix}Contact")
-		end
+		it { should have_content 'Contact' }
+		it { should have_title "#{@site_title_prefix}Contact" }
 	end
 end
