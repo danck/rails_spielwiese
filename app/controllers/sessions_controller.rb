4163f7ae513f6d385707f3c540ab5498ee4900d2
Daniel Kirchner <daniel.c.kirchner@googlemail.com>
2013-08-04 21:02:46 +0200

    adding user sign-in-out system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:email].downcase)
 		if user && user.authenticate(params[:password])
 			sign_in user
+			redirect_back_or user
 			flash[:success] = "Welcome to the Sample App!"
 			redirect_to user
 		else
